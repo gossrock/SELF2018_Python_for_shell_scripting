e0013f5f755ae501244812834e01a4ecfdabd341
Peter Goss <gossrock@gmail.com>
2018-05-28 21:22:09 +0000

    replaced cli api with import statement

--- Step2_parsing.py
+++ Step2_parsing.py
@@ -3,30 +3,7 @@ from subprocess import PIPE
 
 from typing import NamedTuple, IO, Any, List, Optional
 
-######### COMAND LINE API ##############
-class CommandResults(NamedTuple):
-    command: str
-    return_code: int
-    stdout: str
-    stderr: str
-
-def run_command(command: str, stdin: Optional[str] = None, encoding: str = 'utf-8') -> CommandResults:
-    if stdin == None:
-        process = subprocess.Popen(shlex.split(command), stdout=PIPE, stderr=PIPE, encoding=encoding)
-        stdout, stderr = process.communicate()
-    else:
-        process = subprocess.Popen(shlex.split(command), stdin=PIPE, stdout=PIPE, stderr=PIPE, encoding=encoding)
-        stdout, stderr = process.communicate(stdin)
-    return_code = process.wait()
-    return CommandResults(command, return_code, stdout, stderr)
-
-def run_pipeline(commands: List[str], encoding: str = 'utf-8') -> CommandResults:
-    command_results = run_command(commands[0], encoding=encoding)
-    for command in commands[1:]:
-        command_results = run_command(command, stdin=command_results.stdout, encoding=encoding)
-    return command_results
-
-##############################################
+from basic_cli_api import CommandResults, run_command, run_pipeline
 
 ########## PS parse #############################
 def colapse_spaces(text: str) -> str:
