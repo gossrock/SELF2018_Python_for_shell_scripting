fecdf726edcb3958fe1a5fb78602595ff5b6b3bc
Peter Goss <gossrock@gmail.com>
2018-06-03 14:49:19 +0000

    Fixed some typing. Simplified code in places.

--- basic_cli_api.py
+++ basic_cli_api.py
@@ -4,23 +4,24 @@ import shlex
 from typing import NamedTuple, Optional, List
 
 class CommandResults(NamedTuple):
-    command: str
-    return_code: int
-    stdout: str
-    stderr: str
+    command: Optional[str]
+    return_code: Optional[int]
+    stdout: Optional[str]
+    stderr: Optional[str]
+
+BLANK_RESULTS = CommandResults(None, None, None, None)
 
 def run_command(command: str, stdin: Optional[str] = None, encoding: str = 'utf-8') -> CommandResults:
-    if stdin == None:
-        process = subprocess.Popen(shlex.split(command), stdout=PIPE, stderr=PIPE, encoding=encoding)
-        stdout, stderr = process.communicate()
-    else:
-        process = subprocess.Popen(shlex.split(command), stdin=PIPE, stdout=PIPE, stderr=PIPE, encoding=encoding)
-        stdout, stderr = process.communicate(stdin)
+    process = subprocess.Popen(shlex.split(command), stdin=PIPE, stdout=PIPE, stderr=PIPE, encoding=encoding)
+    stdout, stderr = process.communicate(stdin)
     return_code = process.wait()
     return CommandResults(command, return_code, stdout, stderr)
 
 def run_pipeline(commands: List[str], encoding: str = 'utf-8') -> CommandResults:
-    command_results = run_command(commands[0], encoding=encoding)
-    for command in commands[1:]:
+    command_results = BLANK_RESULTS
+    for command in commands:
         command_results = run_command(command, stdin=command_results.stdout, encoding=encoding)
     return command_results
+
+def pipe(command1: str, command2: str, encoding: str = 'utf-8') -> CommandResults:
+    return run_pipeline([command1, command2], encoding=encoding)
