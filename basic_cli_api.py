acce69a84a7b259ad52074447652ec54c8bd03a6
Peter Goss <gossrock@gmail.com>
2018-06-03 15:20:32 +0000

    yet another simplification. I realized that pipe was now no longer useful, run_command could be made easier to type and that run_pipeline really wasn't needed buy an end user anymore.

--- basic_cli_api.py
+++ basic_cli_api.py
@@ -11,22 +11,19 @@ class CommandResults(NamedTuple):
 
 BLANK_RESULTS = CommandResults(None, None, None, None)
 
-def run_command(command: str, stdin: Optional[str] = None, encoding: str = 'utf-8') -> CommandResults:
+def run(command: str, stdin: Optional[str] = None, encoding: str = 'utf-8') -> CommandResults:
     command = command.strip()
     if '|' in command:
-        return run_pipeline(command.split('|'))
+        return _run_pipeline(command.split('|'))
     else:
         process = subprocess.Popen(shlex.split(command), stdin=PIPE, stdout=PIPE, stderr=PIPE, encoding=encoding)
         stdout, stderr = process.communicate(stdin)
         return_code = process.wait()
         return CommandResults(command, return_code, stdout, stderr)
 
-def run_pipeline(commands: List[str], stdin: Optional[str] = None, encoding: str = 'utf-8') -> CommandResults:
+def _run_pipeline(commands: List[str], stdin: Optional[str] = None, encoding: str = 'utf-8') -> CommandResults:
     command_results = BLANK_RESULTS
     for command in commands:
-        command_results = run_command(command, stdin=stdin, encoding=encoding)
+        command_results = run(command, stdin=stdin, encoding=encoding)
         stdin = command_results.stdout
     return command_results
-
-def pipe(command1: str, command2: str, encoding: str = 'utf-8') -> CommandResults:
-    return run_pipeline([command1, command2], encoding=encoding)
