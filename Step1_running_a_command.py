1294a069febf92bb8c63358483c1e13565d517df
Peter Goss <gossrock@gmail.com>
2018-06-07 11:59:13 -0400

    simplified for presentation purposes

--- Step1_running_a_command.py
+++ Step1_running_a_command.py
@@ -1,14 +1,6 @@
 import subprocess, shlex
 from subprocess import PIPE, CompletedProcess
 
-ls = 'ls'
-ls_la = 'ls -la'
-ping_google_once = 'ping -c 1 google.com'
-list_python_processes = "ps aux | grep 'python'"
-ps_aux = 'ps aux'
-grep_python = "grep 'python'"
-
-
 def print_run_results(results: CompletedProcess) -> None:
     print('\nARGS')
     print(f'{results.args}')
@@ -40,17 +32,26 @@ def simplest_run(command: str) -> None:
     results = subprocess.run(shlex.split(command))
     print_run_results(results)
 
+#simplest_run('ls')
+
 def capture_stdout_and_stderr_run(command: str) -> None:
     results = subprocess.run(shlex.split(command), stdout=PIPE, stderr=PIPE)
     print_run_results(results)
+
+#capture_stdout_and_stderr_run('ls -la')
 
 def make_strings_not_bytes_run(command: str) -> None:
     results = subprocess.run(shlex.split(command), stdout=PIPE, stderr=PIPE, encoding='utf-8')
     print_run_results(results)
 
+#make_strings_not_bytes_run('ping -c 1 google.com')
+#make_strings_not_bytes_run("ps aux | grep 'python'") # fails to run properly
+
 def execute_with_bash_run(command: str) -> None:
     results = subprocess.run(command, stdout=PIPE, stderr=PIPE, encoding='utf-8', shell=True, executable='/bin/bash')
     print_run_results(results)
+
+#execute_with_bash_run("ps aux | grep 'python'")
 
 # subprocess.Popen variations
 def pipe_without_using_bash(command1: str, command2: str) -> None:
@@ -59,6 +60,8 @@ def pipe_without_using_bash(command1: str, command2: str) -> None:
     return_code = process2.wait()
     stdout, stderr = process2.communicate()
     print_Popen_results(f'{command1} | {command2}', return_code, stdout, stderr)
+
+#pipe_without_using_bash('ps aux', "grep 'python'")
 
 def pipe_without_using_bash2(command1: str, command2: str) -> None:
     process1 = subprocess.Popen(shlex.split(command1), stdin=None, stdout=PIPE, stderr=PIPE, encoding='utf-8')
@@ -71,13 +74,4 @@ def pipe_without_using_bash2(command1: str, command2: str) -> None:
 
     print_Popen_results(f'{command1} | {command2}', p2_return_code, p2_stdout, p2_stderr)
 
-
-
-if __name__ == '__main__':
-    #simplest_run(ls)
-    #capture_stdout_and_stderr_run(ls_la)
-    #make_strings_not_bytes_run(ping_google_once)
-    #make_strings_not_bytes_run(list_python_processes) # fails to run properly
-    #execute_with_bash_run(list_python_processes)
-    #pipe_without_using_bash(ps_aux, grep_python)
-    pipe_without_using_bash2(ps_aux, grep_python)
+#pipe_without_using_bash2('ps aux', "grep 'python'")
